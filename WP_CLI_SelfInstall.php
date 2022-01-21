73d0dbcc869e94470b8407b23aa01fb29199011e
A-Perrier <perrier_anthony@live.fr>
2022-01-21 18:18:51 +0100

    Passage en non-statique, création d'un user en BDD lors de la création, refactorisation

--- WP_CLI_SelfInstall.php
+++ WP_CLI_SelfInstall.php
@@ -11,24 +11,29 @@ class WP_CLI_SelfInstall
     private const WP_CORE_DL_COMMAND = 'wp core download ';
     private const WP_CONFIG_COMMAND = 'wp config create ';
     private const WP_CORE_INSTALL_COMMAND = 'wp core install ';
-    private static $config_mode;
-    private static $install_path;
-    private static $locale = 'fr_FR';
-    private static $db_name;
-    private static $db_user;
-    private static $db_pass;
-    private static $site_url;
-    private static $site_title = "Mon site WordPress installé avec WP_CLI_SelfInstall !";
-    private static $admin_user;
-    private static $admin_pass;
-    private static $admin_email;
+    private $config_mode;
+    private $install_path;
+    private $locale = 'fr_FR';
+    private $db_name;
+    private $db_user;
+    private $db_pass;
+    private $site_url;
+    private $site_title = "Mon site WordPress installé avec WP_CLI_SelfInstall !";
+    private $admin_user;
+    private $admin_pass;
+    private $admin_email;
 
-    public static function process ()
+    public function __construct()
+    {
+        $this->process();
+    }
+
+    private function process ()
     {
         echo "\nBienvenue dans l'assistant de configuration WordPress !\n";
         echo "Pour installer WordPress correctement, nous allons devoir répondre à quelques questions.\n\n";
     
-        self::installWordPressFully();
+        $this->installWordPressFully();
     }
 
 
@@ -36,7 +41,7 @@ class WP_CLI_SelfInstall
      * <! Inutilisée pour le moment !>
      * Récupère le mode de configuration demandé par l'utilisateur.ice
      */
-    private static function askConfigMode (): void
+    private function askConfigMode (): void
     {
         echo "Tout d'abord, choisissez le mode d'installation : [Par défaut : 2]\n";
         echo " 1 - Créer et installer WordPress uniquement\n";
@@ -49,218 +54,251 @@ class WP_CLI_SelfInstall
 
         if (trim($mode) !== "1" && trim($mode) !== "2") {
             echo "\n\n   Warning /!\ $mode n'est pas une valeur correcte.\n\n\n";
-            self::askConfigMode();
+            $this->askConfigMode();
             return;
         }
 
-        self::$config_mode = $mode;
+        $this->config_mode = $mode;
     }
 
 
     /**
      * Dirige le mode de configuration d'installation de WordPress seul
      */
-    private static function installWordPressFully ()
+    private function installWordPressFully ()
     {
         echo "\n Vous avez choisi l'installation de WordPress uniquement. Voyons ensemble comment créer tout ça\n";
         
         // Récupération des données pour la commande `wp core download`
-        self::getInstallPath();
+        $this->getInstallPath();
 
         // Récupération des données pour la commande `wp config`
-        self::getDbName();
-        self::getDbUser();
-        self::getDbPass();
+        $this->getDbName();
+        $this->getDbUser();
+        $this->getDbPass();
 
         // Récupération des données pour la commande `wp core install`
-        self::getSiteUrl();
-        self::getSiteTitle();
-        self::getAdminUser();
-        self::getAdminPass();
-        self::getAdminEmail();
+        $this->getSiteUrl();
+        $this->getSiteTitle();
+        $this->getAdminUser();
+        $this->getAdminPass();
+        $this->getAdminEmail();
 
-        // Creation de la base de données
-        shell_exec("mysql -h localhost -u " . self::$db_user . " -p" . self::$db_pass . " -e 'CREATE DATABASE ". self::$db_name . "'");
+        // Création de la base de données et récupération des droits via un compte MySQL
+        $this->createDatabaseAndNewUser();
+        
+
+        // Création du Virtual Host
+        $this->createVirtualHost();
+        $this->registerVirtualHost();
+
+        
+        if ($this->install_path) {
+            shell_exec("cd " . $this->install_path);
+        }
 
         // Ouverture des droits aux fichiers pour contourner le bug du wp-config manquant
-        if (self::$install_path) {
-            shell_exec("cd " . self::$install_path);
-        }
-        shell_exec("sudo chmod -R 777 .*");
-
+        # $this->upgradeRights();
 
         // Envoi des requêtes par la WP_CLI
-        self::downloadWPCore();
-        self::configureWP();
-        self::installWPCore();
+        $this->downloadWPCore();
+        $this->configureWP();
+        $this->installWPCore();
 
         // On confirme le succès des opérations
-        echo "\n\n\n Félicitations ! WordPress est correctement installé et la base de données configurée ! Si vous avez déjà créé votre Virtual Host et rechargé Apache, rendez-vous sur http://" . self::$site_url . " !\n\n\n";
+        echo "\n\n\n Félicitations ! WordPress est correctement installé et la base de données configurée ! Si vous avez déjà créé votre Virtual Host et rechargé Apache, rendez-vous sur http://" . $this->site_url . " !\n\n\n";
     }
 
 
-    private static function getInstallPath (): void
+    private function getInstallPath (): void
     {
         echo " Dans quel dossier souhaitez-vous installer votre projet ? [Par défaut : dossier courant]\n";
         $path = readline(">>> ");
 
         if (strlen(trim($path)) !== 0) {
-            self::$install_path = trim($path);
+            $this->install_path = trim($path);
         }
     }
 
 
 
-    private static function getDbName (): void
+    private function getDbName (): void
     {
         echo "\n Comment souhaitez-vous nommer votre base de données ?\n";
         $dbname = readline(">>> ");
-
+        
         if (strlen(trim($dbname)) === 0) {
             echo "\n\n   Warning /!\ Vous devez avoir un nom de base de données.\n\n\n";
-            self::getDbName();
+            $this->getDbName();
             return;
         }
 
-        self::$db_name = $dbname;
+        $this->db_name = $dbname;
     }
 
 
-    private static function getDbUser (): void
+    private function getDbUser (): void
     {
         echo "\n Quel est le nom utilisateur lié à votre base de données ?\n";
         $dbuser = readline(">>> ");
-
+        
         if (strlen(trim($dbuser)) === 0) {
             echo "\n\n   Warning /!\ Vous devez avoir un nom d'utilisateur.\n\n\n";
-            self::getDbUser();
+            $this->getDbUser();
             return;
         }
 
-        self::$db_user = $dbuser;
+        $this->db_user = $dbuser;
     }
 
 
-    private static function getDbPass (): void
+    private function getDbPass (): void
     {
         echo "\n Quel est le mot de passe d'accès à votre base de données ?\n";
         $dbpass = readline_hidden(">>> ");
-
-        self::$db_pass = $dbpass;
+        
+        $this->db_pass = $dbpass;
     }
 
     
-    private static function getSiteUrl (): void
+    private function getSiteUrl (): void
     {
         echo "\n Quel est l'URL de votre site ? (doit être la même que celle du VirtualHost)\n";
         $url = readline(">>> ");
-
+        
         if (strlen(trim($url)) === 0) {
             echo "\n\n   Warning /!\ Vous devez avoir une URL pour votre site.\n\n\n";
-            self::getSiteUrl();
+            $this->getSiteUrl();
             return;
         }
 
-        self::$site_url = $url;
+        $this->site_url = $url;
     }
 
 
-    private static function getSiteTitle (): void
+    private function getSiteTitle (): void
     {
-        echo "\n Quel est le titre de votre site ? [Par défaut: " . self::$site_title . "]\n";
+        echo "\n Quel est le titre de votre site ? [Par défaut: " . $this->site_title . "]\n";
         $title = readline(">>> ");
 
         if (strlen(trim($title)) === 0) {
             return;
         }
 
-        self::$site_title = $title;
+        $this->site_title = $title;
     }
 
     
-    private static function getAdminUser (): void
+    private function getAdminUser (): void
     {
         echo "\n Quel est le nom d'utilisateur que vous souhaitez utiliser pour vous connecter ?\n";
         $username = readline(">>> ");
-
+        
         if (strlen(trim($username)) === 0) {
             echo "\n\n   Warning /!\ Vous devez avoir un nom d'utilisateur pour votre site.\n\n\n";
-            self::getAdminUser();
+            $this->getAdminUser();
             return;
         }
 
-        self::$admin_user = $username;
+        $this->admin_user = $username;
     }
 
 
-    private static function getAdminPass (): void
+    private function getAdminPass (): void
     {
         echo "\n Quel est le mot de passe avec lequel vous souhaitez pouvoir vous connecter ?\n";
         $pass = readline_hidden(">>> ");
-
+        
         if (strlen(trim($pass)) === 0) {
             echo "\n\n   Warning /!\ Vous devez avoir un mot de passe pour votre compte WordPress !\n\n\n";
-            self::getAdminPass();
+            $this->getAdminPass();
             return;
         }
 
-        self::$admin_pass = $pass;
+        $this->admin_pass = $pass;
     }
 
 
-    private static function getAdminEmail (): void
+    private function getAdminEmail (): void
     {
         echo "\n Quel est l'email qui sera lié à votre site ?\n";
         $email = readline(">>> ");
-
+        
         if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
             echo "\n\n   Warning /!\ $email n'est pas une adresse email valide.\n\n\n";
-            self::getAdminEmail();
+            $this->getAdminEmail();
             return;
         }
 
         if (strlen(trim($email)) === 0) {
             echo "\n\n   Warning /!\ Vous devez avoir une adresse email à lier à votre site.\n\n\n";
-            self::getAdminEmail();
+            $this->getAdminEmail();
             return;
         }
 
-        self::$admin_email = $email;
+        $this->admin_email = $email;
+    }
+
+    
+    private function createDatabaseAndNewUser (): void
+    {
+        shell_exec("sudo mysql -h localhost -u {$this->db_user} -p{$this->db_pass} -e \"DROP DATABASE IF EXISTS {$this->db_name}; CREATE DATABASE {$this->db_name}; GRANT ALL PRIVILEGES ON {$this->db_name}.* TO '{$this->db_name}'@'localhost' IDENTIFIED BY '{$this->db_name}';\"");
     }
 
 
-    private static function downloadWPCore (): void
+    private function upgradeRights (): void
     {
-        $command = self::$install_path === null ? 
-            self::WP_CORE_DL_COMMAND . ' --locale=' . self::$locale :
-            self::WP_CORE_DL_COMMAND . ' --locale=' . self::$locale .  ' --path=' . self::$install_path;
+        shell_exec("sudo chgrp -R www-data");
+        shell_exec("sudo find . -type f -exec chmod 664 {} +");
+        shell_exec("sudo find . -type d -exec chmod 775 {} +");
+    }
+
+
+    private function createVirtualHost (): void
+    {
+
+    }
+
+
+    private function registerVirtualHost (): void
+    {
+
+    }
+
+
+    private function downloadWPCore (): void
+    {
+        var_dump('ici');
+        $command = $this->install_path === null ? 
+            self::WP_CORE_DL_COMMAND . ' --locale=' . $this->locale :
+            self::WP_CORE_DL_COMMAND . ' --locale=' . $this->locale .  ' --path=' . $this->install_path;
         shell_exec($command);
     }
 
 
-    private static function configureWP (): void
+    private function configureWP (): void
     {
         $command = 
             self::WP_CONFIG_COMMAND . 
-            ' --dbname=' . self::$db_name .
-            ' --dbuser=' . self::$db_user .
-            ' --dbpass=' . self::$db_pass . 
-            ' --prompt=' . self::$db_pass
+            ' --dbname=' . $this->db_name .
+            ' --dbuser=' . $this->db_user .
+            ' --dbpass=' . $this->db_pass . 
+            ' --prompt=' . $this->db_pass
         ;
         shell_exec($command);
     }
 
 
-    private static function installWPCore (): void
+    private function installWPCore (): void
     {
         
         $command = 
         self::WP_CORE_INSTALL_COMMAND . 
-        ' --url="'               . self::$site_url   .
-        '" --title="'             . self::$site_title .
-        '" --admin_user='        . self::$admin_user .
-        ' --admin_password='    . self::$admin_pass . 
-        ' --admin_email='       . self::$admin_email
+        ' --url="'               . $this->site_url   .
+        '" --title="'            . $this->site_title .
+        '" --admin_user='        . $this->admin_user .
+        ' --admin_password='     . $this->admin_pass . 
+        ' --admin_email='        . $this->admin_email
         ;
         shell_exec($command);
     }
@@ -277,4 +315,4 @@ function readline_hidden ($prompt) {
 
 
 
-WP_CLI_SelfInstall::process();
+(new WP_CLI_SelfInstall);
